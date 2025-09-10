90f5ec92ea5aed4447879429d2fe1c0eddf10417
Juned Ahmed Choudhary <jjahmedchoudhary@gmail.com>
2025-09-10 22:15:01 +0530

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req: Request, res: Response) => {
-  return res.send("It's working 🙌");
+  return res.send("It's asfasf 🙌");
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
