8b476a6e86f8dda166656ca777a9a8d942b12767
Juned Ahmed Choudhary <jjahmedchoudhary@gmail.com>
2025-10-21 11:05:10 +0530

    redis setuping

--- src/config/alerting.ts
+++ src/config/alerting.ts
@@ -3,14 +3,12 @@ import { ALERTS } from "../constants/alertEnums.js";
 const config: any = {
   local: {
     slack: {
-      [ALERTS.ALERT_TESTING]:
-        `https://hooks.slack.com/services/${process.env.SLACK_ALERT_SUFFIX}`,
+      [ALERTS.ALERT_TESTING]: `https://hooks.slack.com/services/${process.env.SLACK_ALERT_SUFFIX}`,
     },
   },
   production: {
     slack: {
-      [ALERTS.ALERT_TESTING]:
-        `https://hooks.slack.com/services/${process.env.SLACK_ALERT_SUFFIX}`,
+      [ALERTS.ALERT_TESTING]: `https://hooks.slack.com/services/${process.env.SLACK_ALERT_SUFFIX}`,
     },
   },
 };
