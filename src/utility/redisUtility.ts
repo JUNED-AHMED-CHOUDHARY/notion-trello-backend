a2aa673ccbf0ac0b53fcf7ca2f9fcb0c03677000
JUNED-AHMED-CHOUDHARY <144229990+JUNED-AHMED-CHOUDHARY@users.noreply.github.com>
2025-12-03 23:17:36 +0530

    redis utilities added for work (#8)
    * redis utilities added for work
    
    * redis utilities added for work

---
+++ src/utility/redisUtility.ts
@@ -0,0 +1,25 @@
+import redisClient from "../setup/redis.setup.js";
+const DEFAULT_TTL = 24 * 60 * 60; // 24 hours in seconds
+const APP_PREFIX = "notion-trello";
+
+export const getDataFromRedis = async (redisKey: string): Promise<string | null> => {
+  try {
+    const key = `${APP_PREFIX}:${redisKey}`;
+    let data = await redisClient.get(key);
+    if (data) data = JSON.parse(data);
+    return data;
+  } catch (error) {
+    console.error(`Error getting data from Redis for key ${redisKey}:`, error);
+    throw error;
+  }
+};
+
+export const storeInCacheWithTTL = async (redisKey: string, data: string, ttl: number = DEFAULT_TTL) => {
+  try {
+    const key = `${APP_PREFIX}:${redisKey}`;
+    return await redisClient.setex(key, ttl, JSON.stringify(data));
+  } catch (error) {
+    console.error(`Error setting data in Redis for key ${redisKey}:`, error);
+    throw error;
+  }
+};
